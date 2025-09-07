d09d9a01acdef44c2f3644e92e87da4e5eb9a29b
EdithTech <yash9atalkar@gmail.com>
2025-09-07 08:58:24 +0530

    fix path

--- src/Config/AppRoutes.jsx
+++ src/Config/AppRoutes.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import App from "../App";
 import JoinRoom from "../pages/room/JoinRoom.jsx";
 import Signup from "../pages/signup/Signup.jsx";
-import Chat from "../pages/chat/Chat.jsx";
+import Chat from "../pages/chat/Chat";
 
 const AppRoutes = () => {
   return (
